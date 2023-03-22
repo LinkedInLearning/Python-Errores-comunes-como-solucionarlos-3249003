9a88f2e9055c283c6ff2ce7958b5cd49de152f5e
Ana Maria Pinto <47826699+apinto25@users.noreply.github.com>
2023-03-22 10:47:37 +0000

    Update c2

--- c2_sintaxis/c2v05_indentacion.py
+++ c2_sintaxis/c2v05_indentacion.py
@@ -5,13 +5,13 @@ class Persona:
 
     def cumplir_anios(self):
         self.edad += 1
-        print(f"Feliz Cumpleaños #{self.edad} {self.nombre}")
+        print(f"Feliz Cumpleaños")
 
     def es_mayor_edad(self):
         es_mayor_edad = False
         if self.edad >= 18:
             es_mayor_edad = True
-            return es_mayor_edad
+        return es_mayor_edad
 
 
 paco = Persona(nombre="Paco", edad=27)
