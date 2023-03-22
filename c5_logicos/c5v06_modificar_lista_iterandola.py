1fffb4e0002cf965edc2a9645afbfbf617b06012
Ana Maria Pinto <47826699+apinto25@users.noreply.github.com>
2023-03-22 15:19:26 +0000

    Update c5

--- c5_logicos/c5v06_modificar_lista_iterandola.py
+++ c5_logicos/c5v06_modificar_lista_iterandola.py
@@ -1,15 +1,3 @@
-# Iteración y modificación de una lista
-lista_numeros = [2, 5, 8, 12, 14, 35]
-
-for numero in lista_numeros:
-    print(numero)
-    if numero % 2 == 0:
-        lista_numeros.remove(numero)
-
-print(lista_numeros)
-
-
-# Iteración correcta de una lista
 lista_numeros = [2, 5, 8, 12, 14, 35]
 
 impares = []
@@ -17,4 +5,4 @@ for numero in lista_numeros:
     if numero % 2 != 0:
         impares.append(numero)
 
-print(lista_numeros)
+print(impares)
--- c5_logicos/c5v07_multiples_lineas_codigo.py
+++ c5_logicos/c5v07_multiples_lineas_codigo.py
@@ -1,41 +1,36 @@
 def recibir_datos(datos):
-
     # Bloque de código relacionado a la recepción de los datos
     # Línea 1
     # Línea 2
     # Línea 3
     # Línea 4
     # Línea 5
-
     return datos
 
 
 def limpiar_datos(datos):
-
     # Bloque de código relacionado a la limpieza de los datos
     # Línea 1
     # Línea 2
     # Línea 3
     # Línea 4
     # Línea 5
-
     return datos
 
 
 def procesar_datos(datos):
-
     # Bloque de código relacionado al procesamiento de datos
     # Línea 1
     # Línea 2
     # Línea 3
     # Línea 4
     # Línea 5
-
     return datos
 
 
 def obtener_datos(datos):
 
-    datos_procesados = recibir_datos(datos)
-    datos_procesados = limpiar_datos(datos)
-    datos_procesados = procesar_datos(datos)
+    datos_recibidos = recibir_datos(datos)
+    datos_limpios = limpiar_datos(datos_recibidos)
+    datos_procesados = procesar_datos(datos_limpios)
+    return datos_procesados
